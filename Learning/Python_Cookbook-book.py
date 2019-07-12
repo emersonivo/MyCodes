0828c6003cdc37fb4a6a1861ea49066b96dc6772
emersonivo <emersonivo@gmail.com>
2019-07-12 11:43:06 -0400

    201907120742AM Page 38/706

--- Learning/Python_Cookbook-book.py
+++ Learning/Python_Cookbook-book.py
@@ -42,8 +42,63 @@ class _Book():
             line = 'nobody:*:-2:-2:Unprivileged User:/var/empty:/usr/bin/false'
             uname, *fields, homedir, sh = line.split(':')
             print("#C1.2.4 - fields ", fields)
+        
+        def Keep_N():
+            print("#C1.3 - Page 23/706")
+            from collections import deque
+            def search(lines, pattern, history=5):
+                previous_lines = deque(maxlen=history)
+                for line in lines:
+                    if pattern in line:
+                        yield line, previous_lines
+                    previous_lines.append(line)
+                # print(previous_lines)
+                # previous_lines.appendleft("appendleft")
+                # print(previous_lines)
+                # previous_lines.append("appendright")
+                # print('*'*20)
+
+            if __name__ == '__main__':
+                with open('somefile.txt') as f:
+                    for line, prevlines in search(f, 'python', 5):
+                        for pline in prevlines:
+                            print(pline, end='')
+                        print(line, end='')
+                        print('-'*20)
+
+        def find_Largest_Smallest():
+            print("#C1.4 - Page 25/706")
+            import heapq
+            portfolio = [
+                {'name': 'IBM', 'shares': 100, 'price': 91.1},
+                {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+                {'name': 'FB', 'shares': 200, 'price': 21.09},
+                {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+                {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+                {'name': 'ACME', 'shares': 75, 'price': 115.65}
+                ]
+            cheap = heapq.nsmallest(2, portfolio, key=lambda s: s['price'])
+            expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
+            print(cheap)
+            print(expensive)
+        
+        def Remove_Duplicates():
+            def dedupe(items):
+                seen = set()
+                for item in items:
+                    if item not in seen:
+                        yield item
+                        seen.add(item)
+
+            a = [1, 2, 5, 1, 9, 1, 5, 10]
+            print("#C1.10.1 ", list(dedupe(a)))
 
         #Sequence() #Page 19/706
-        Arbitrary_Length()
+        #Arbitrary_Length()
+        #Keep_N()
+        #find_Largest_Smallest()
+        Remove_Duplicates()
+
 _Book.Chapter1()
+
 
